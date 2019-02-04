d8e39983b7348e464c2614fd0b55a263583ab378
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-02-04 02:35:05 +0200

    Update domainker.py

--- domainker.py
+++ domainker.py
@@ -150,11 +150,11 @@ print (colorama.Fore.LIGHTBLACK_EX  + "|______| |_______||_|   |_||__| |__||___|
 
 parser = argparse.ArgumentParser(description='Welcome to domainker help page')
 
-parser.add_argument('-d','--domains'			,type=str, help='Domains list')
-parser.add_argument('-a','--aws'				,action='store_true', help='Perform aws checking')
-parser.add_argument('-o','--output'				,type=str, help='Output file')
-parser.add_argument('-t','--threads'			,type=int, help='Number of threads default=20',default=10)
-parser.add_argument('-T','--thread-timeout'		,type=int, help='Threads timeout default=30',default=30)
+parser.add_argument('-d','--domains'		,type=str, help='Domains list')
+parser.add_argument('-a','--aws'		,action='store_true', help='Perform aws checking')
+parser.add_argument('-o','--output'		,type=str, help='Output file')
+parser.add_argument('-t','--threads'		,type=int, help='Number of threads default=20',default=10)
+parser.add_argument('-T','--thread-timeout'	,type=int, help='Threads timeout default=30',default=30)
 parser.add_argument('-rt','--request-timeout'	,type=int, help='Request timeout default=30',default=30)
 
 args = parser.parse_args()

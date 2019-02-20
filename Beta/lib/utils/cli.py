fcd6a2af7d19baeed74496f255fff22c17654778
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-02-20 09:08:26 +0200

    Update cli.py

--- Beta/lib/utils/cli.py
+++ Beta/lib/utils/cli.py
@@ -1,21 +1,36 @@
 from __future__ import print_function
+from threading import Lock
 import colorama
+import re
 import sys
 
+log = ""
+lock = Lock()
 colorama.init(autoreset=True)
 
 def pprint(**kw):
+	global log
+
 	fmt = "\n[!] %s%s%s\n"% (colorama.Fore.LIGHTWHITE_EX,kw['HOST'],colorama.Fore.RESET)
 	for key in kw:
 		if kw[key] != None and key != 'HOST' and not str(key).startswith('__'):
 			fmt += " |_> [{}]: {}\n".format(key,kw[key])
 	fmt += "%s" % colorama.Fore.RESET
 
+	with lock:
+		log += fmt
 	print(fmt,end='')
 
 def no_options():
 	print('[ERR] Please choose a module')
 	sys.exit(0)
+
+def escape_ansi(line):
+    ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+    return ansi_escape.sub('', line)
+
+def save_log(path):
+	open(path,"w").write(escape_ansi(log))
 
 def start_pause():
 	raw_input(colorama.Fore.YELLOW + "[!] Press enter to start ")

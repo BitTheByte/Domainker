3e3bc37bbc14587b1af3c117065ec27054f57d51
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-02-20 11:11:32 +0200

    Update aws.py

--- lib/modules/aws.py
+++ lib/modules/aws.py
@@ -8,9 +8,9 @@ def ckaws(url,timeout=60):
 	try:
 		aws = requests.get("http://%s.s3.amazonaws.com" % url,timeout=timeout)
 		if aws.status_code == 404:
-			return '%sNot hosted on AWS' % colorama.Fore.RED
+			return '%sNot hosted on AWS%s' % (colorama.Fore.RED,colorama.Fore.RESET)
 		
-		return '%sHosted on AWS %s-> %s%s' % (colorama.Fore.GREEN,colorama.Fore.YELLOW,colorama.Fore.LIGHTWHITE_EX,"http://%s.s3.amazonaws.com" % url)
+		return '%sHosted on AWS %s-> %s%s%s' % (colorama.Fore.GREEN,colorama.Fore.YELLOW,colorama.Fore.LIGHTWHITE_EX,"http://%s.s3.amazonaws.com" % url,colorama.Fore.RESET)
 
 	except:
 		pass

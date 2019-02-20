7508cf51dee4b8b933395fab89b20531f495f555
root <root@bitthebyte-pc.localdomain>
2019-02-20 09:45:53 +0200

    Beta release

--- Beta/lib/modules/cname.py
+++
@@ -1,8 +0,0 @@
-from dns import resolver
-
-def chkcname(host):
-	try:
-		answers = resolver.query(host, 'CNAME')
-		return ','.join([str(rdata.target) for rdata in answers])
-	except:
-		return "NaN"

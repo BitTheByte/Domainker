3b4a69e9a32a25a2a6d79eaa1ee6fb81eb84950b
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-02-22 00:44:43 +0200

    Update aws.py

--- lib/modules/aws.py
+++ lib/modules/aws.py
@@ -2,6 +2,8 @@ from cStringIO import StringIO
 import requests
 import colorama 
 import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
 from urllib3.exceptions import InsecureRequestWarning 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
@@ -13,13 +15,7 @@ upload_body = """<html>
 </html>"""
 
 def tkaws(bucket):
-	session = boto3.Session(
-	    aws_access_key_id="XXXXXXXXXXXXXXXXXXXXXXXXX", # Your aws key
-	    aws_secret_access_key="XXXXXXXXXXXXXXXXXXXXX", # Your aws key
-	)
-
-	s3 = session.client('s3')
-	
+	s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 	try:
 		s3.put_object(Bucket=bucket, Key=upload_name,ACL='public-read', Body=StringIO(upload_body).read())
 		response = requests.get("http://{bucket}.s3.amazonaws.com/{target}".format(bucket=bucket,target=upload_name)).content

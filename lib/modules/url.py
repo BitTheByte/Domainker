b3e34a0db08db6092f87fade6f590e237de8ed0e
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-03-02 20:48:33 +0200

    Update url.py

--- lib/modules/url.py
+++ lib/modules/url.py
@@ -6,7 +6,7 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 def ckurl(url,timeout=60):
 	try:
-		res = str(requests.head(url,timeout=timeout,verify=False).status_code)
+		res = str(requests.get(url,timeout=timeout,verify=False).status_code)
 
 		if res[0] == "2": return colorama.Fore.GREEN  + res + colorama.Fore.RESET     
 		if res[0] == "3": return colorama.Fore.YELLOW + res + colorama.Fore.RESET      

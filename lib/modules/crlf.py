e2b157ad02eaff7da9f15727d35d52cd7cf17d03
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-03-02 21:02:36 +0200

    Update crlf.py

--- lib/modules/crlf.py
+++ lib/modules/crlf.py
@@ -42,4 +42,4 @@ def chkcrlf(url,timeout=30):
         return safe_msg % (Fore.RED)
 
     except Exception as e:
-        return "%sError occured: %s" % (Fore.RED,str(e))
+        return "%sError occured" % (Fore.RED)

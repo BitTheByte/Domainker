7f1e458b1ac1011345f2c5cd3857841f16c3a365
k3r1it0 <39193373+k3r1it0@users.noreply.github.com>
2019-03-02 21:13:01 +0200

    Update crlf.py

--- lib/modules/crlf.py
+++ lib/modules/crlf.py
@@ -42,4 +42,4 @@ def chkcrlf(url,timeout=30):
         return safe_msg % (Fore.RED)
 
     except Exception as e:
-        return "%sError occured" % (Fore.RED)
+        return "%sDown Host" % (Fore.RED)

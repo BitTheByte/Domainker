11c5408f527b6ad4bcd6b59e6d17f5e3c9a753cc
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-03-02 21:46:43 +0200

    Restored

--- lib/modules/crlf.py
+++ lib/modules/crlf.py
@@ -1,1 +1,45 @@
-test
+from colorama import Fore
+import requests
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
+
+vuln_msg = "%sVulnerable %s-> %s%s%s"
+safe_msg = "%sNot vulnerable"
+inject_header  = "controlled-header"
+inject_value   = "controlled-value"
+inject_payload = "/%0D%0A"
+
+def chkcrlf(url,timeout=30):
+
+    try:
+        url = url + "%s%s:%s" % (inject_payload,inject_header,inject_value)
+
+        request = requests.get(url, timeout=timeout,verify=False)
+
+        for header_name, header_value in request.headers.items():
+            if header_value == inject_value:
+                return vuln_msg % (
+                        Fore.GREEN,
+                        Fore.YELLOW,
+                        Fore.LIGHTWHITE_EX,
+                        url,
+                        Fore.RESET
+                    )
+
+
+        if request.history:
+            for history in request.history:
+                for header_name, header_value in history.headers.items():
+                    if header_value == inject_value:
+                        return vuln_msg % (
+                                Fore.GREEN,
+                                Fore.YELLOW,
+                                Fore.LIGHTWHITE_EX,
+                                url,
+                                Fore.RESET
+                            )
+        
+        return safe_msg % (Fore.RED)
+
+    except Exception as e:
+        return "%sUnreachable" % (Fore.RED)

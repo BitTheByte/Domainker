32a95db48f2d83f70ee57c9ce52415bd7cccc1b3
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-03-05 06:17:10 +0200

    Update cli.py

--- lib/utils/cli.py
+++ lib/utils/cli.py
@@ -12,19 +12,19 @@ colorama.init(autoreset=True)
 def pprint(**kw):
 	global log
 	
-	fmt = "\n%s[%s*%s] %s%s%s%s\n"% (
+	fmt = "\n %s[%s*%s] %s%s%s%s\n"% (
 		colorama.Fore.BLUE, colorama.Fore.LIGHTRED_EX,colorama.Fore.BLUE,colorama.Fore.RESET
 		,colorama.Fore.LIGHTWHITE_EX,kw['HOST'],colorama.Fore.RESET)
 	for key in kw:
 		if kw[key] != None and key != 'HOST' and not str(key).startswith('__'):
-			fmt += " |_> [{}]: {}{}\n".format(key,kw[key],colorama.Fore.RESET)
+			fmt += "  |_> [{}]: {}{}\n".format(key,kw[key],colorama.Fore.RESET)
 
 	with lock:
 		log += fmt
 		print(fmt,end='')
 
 def no_options():
-	print('[ERR] Please choose a module')
+	print(' [ERR] Please choose a module')
 	sys.exit(0)
 
 def escape_ansi(line):
@@ -35,24 +35,30 @@ def save_log(path):
 	open(path,"w").write(escape_ansi(log))
 
 def start_pause():
-	raw_input(colorama.Fore.YELLOW + "[!] Press enter to start ")
+	raw_input(colorama.Fore.YELLOW + " [!] Press enter to start ")
 
 def end_pause():
-	raw_input("\n"+colorama.Fore.GREEN + "[!] Finished enter to exit ")
+	raw_input("\n"+colorama.Fore.GREEN + " [!] Finished enter to exit ")
 
 
 def info(args):
-	print("[INFO]\n  |_> Number of thread(s): {}\n  |_> Thread(s) timeout: {}\n  |_> Request(s) timeout: {}\n".format(args.threads,args.thread_timeout,args.request_timeout))
+	print(" [INFO]\n  |_> Number of thread(s): {}\n  |_> Thread(s) timeout: {}\n  |_> Request(s) timeout: {}\n".format(args.threads,args.thread_timeout,args.request_timeout))
 
 
 def banner():
-	print (colorama.Fore.YELLOW  + " ______   _______  __   __  _______  ___   __    _  ___   _  _______  ______   ")
-	print (colorama.Fore.YELLOW  + "|      | |       ||  |_|  ||   _   ||   | |  |  | ||   | | ||       ||    _ |  ")
-	print (colorama.Fore.RED     + "|  _    ||   _   ||       ||  |_|  ||   | |   |_| ||   |_| ||    ___||   | ||  ")
-	print (colorama.Fore.RED     + "| | |   ||  | |  ||       ||       ||   | |       ||      _||   |___ |   |_||_ ")
-	print (colorama.Fore.RED     + "| |_|   ||  |_|  ||       ||       ||   | |  _    ||     |_ |    ___||    __  |")
-	print (colorama.Fore.YELLOW  + "|       ||       || ||_|| ||   _   ||   | | | |   ||    _  ||   |___ |   |  | |")
-	print (colorama.Fore.YELLOW  + "|______| |_______||_|   |_||__| |__||___| |_|  |__||___| |_||_______||___|  |_|\n")
-	print (colorama.Fore.LIGHTRED_EX + "            > [ BY ] Ahmed Ezzat (BitTheByte) & Abdulrhman adel (k3r1it0)")
-	print (colorama.Fore.LIGHTBLACK_EX + "            > [ Linkedin ] https://linkedin.com/in/abdulrhmanadel")
-	print (colorama.Fore.LIGHTBLACK_EX + "            > [ Github ] https://github.com/BitTheByte\n")
+	print("""
+  {G} ______{r}                    {R}_{r}      {Y} _                         
+  {G}(______){r}                  {R}(_){r}     {Y}| |                     
+  {G} _     _ {r}{Y} ___  ____  _____ _ ____ | |  _ _____  ____           
+  {G}| |   | /{r}{Y}/ _ \|    \(____ | |  _ \| |_/ ) ___ |/ ___)        
+  {G}| |__/ /{r}{Y}/ |_| | | | / ___ | | | | |  _ (| ____| |           
+  {G}|_____/{r}{Y} \____/|_|_|_\_____|_|_| |_|_| \_)_____)_|{r}
+  """.format(
+			R=colorama.Fore.LIGHTRED_EX,
+			r=colorama.Fore.RESET,
+			G=colorama.Fore.LIGHTGREEN_EX,
+			Y=colorama.Fore.YELLOW
+			))
+	print (colorama.Fore.LIGHTRED_EX + "      > [ BY ] Ahmed Ezzat (BitTheByte) & Abdulrhman adel (k3r1it0)")
+	print (colorama.Fore.LIGHTBLACK_EX + "      > [ Linkedin ] https://linkedin.com/in/abdulrhmanadel")
+	print (colorama.Fore.LIGHTBLACK_EX + "      > [ Github ] https://github.com/BitTheByte\n")

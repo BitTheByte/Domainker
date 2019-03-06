52e13a1a67b45ee60d2ed81f7bcea8d63ad51751
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-03-07 01:21:54 +0200

    Update args.py

--- lib/utils/args.py
+++ lib/utils/args.py
@@ -6,6 +6,7 @@ parser.add_argument('-d','--domains',type=str, help='Domains file')
 parser.add_argument('-i','--input',type=str, help='Single host')
 parser.add_argument('-o','--output',type=str, help='Output file')
 
+parser.add_argument('-H','--headers',action='store_true', help='Check for missing security headers')
 parser.add_argument('-a','--aws',action='store_true', help='Check if target is hosted on Amazon aws')
 parser.add_argument('-c','--crlf',action='store_true', help='Check for CRLF vulnerability')
 parser.add_argument('-q','--dns',action='store_true', help='Get target cname')

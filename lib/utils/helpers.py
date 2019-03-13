60c894bbe50225b11427a4305cd289fe715494c1
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-03-13 13:40:57 +0200

    Update helpers.py

--- lib/utils/helpers.py
+++ lib/utils/helpers.py
@@ -5,10 +5,10 @@ def update(current_version):
 	try:
 		remote_version = float(get("https://raw.githubusercontent.com/BitTheByte/Domainker/master/lib/version").content.strip())
 		if remote_version > current_version:
-			print(" %s[WARNING] %sYou are using an old version from this tool"%(Fore.RED,Fore.LIGHTWHITE_EX))
+			print(" %s[WARNING] %sYou are using an old version of this tool"%(Fore.RED,Fore.LIGHTWHITE_EX))
 	except:
 		pass
-
+of
 def read_file(path):
 	try:
 		with open(path,"r") as file:

d978826806bcc8aa1b33bc7ceacc3262937d3e8f
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-04-07 17:03:09 +0200

    Update args.py

--- lib/utils/args.py
+++ lib/utils/args.py
@@ -6,14 +6,16 @@ parser.add_argument('-d','--domains',type=str, help='Domains file')
 parser.add_argument('-i','--input',type=str, help='Single host')
 parser.add_argument('-o','--output',type=str, help='Output file')
 
+parser.add_argument('-u','--url',action='store_true', help='Get target response code and some additional checks')
 parser.add_argument('-H','--headers',action='store_true', help='Check for missing security headers')
+parser.add_argument('-F','--interesting-files',action='store_true', help='Check interesting files')
+
 parser.add_argument('-a','--aws',action='store_true', help='Check if target is hosted on Amazon aws')
+parser.add_argument('-x','--aws-takeover',action='store_true', help='Takeover aws')
+
 parser.add_argument('-c','--crlf',action='store_true', help='Check for CRLF vulnerability')
 parser.add_argument('-q','--dns',action='store_true', help='Get target cname')
 parser.add_argument('-p','--cache-poisoning',action='store_true', help='Check for cache poisoning')
-parser.add_argument('-u','--url',action='store_true', help='Get target response code and some additional checks')
-parser.add_argument('-x','--aws-takeover',action='store_true', help='Takeover aws')
-
 
 parser.add_argument('-t','--threads',type=int, help='Number of threads default=20',default=10)
 parser.add_argument('-T','--thread-timeout',type=int, help='Threads timeout default=30',default=30)

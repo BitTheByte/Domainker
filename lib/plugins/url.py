417265461df240295ae18e0aff4055299604934a
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2019-05-09 17:38:34 +0200

    Removed spaces

--- lib/plugins/url.py
+++ lib/plugins/url.py
@@ -1,18 +1,14 @@
 from linker import *
 
-
+interesting_files = [f.strip() for f in open("lib/wordlist/interesting_files.txt","r").readlines()]
 FLAG_OVER_HTTP = 101
 FLAG_DIR_LISTING = 103
-
-
-interesting_files = [f.strip() for f in open("lib/wordlist/interesting_files.txt","r").readlines()]
 
 def OK(url):
 	if fetch(url)['code'] == 200:
 		return True
 	return False
 		
-
 def fetch(url,timeout=30):
 	try:
 		request = requests.get(url,timeout=timeout,verify=False)
@@ -40,19 +36,16 @@ def fetch(url,timeout=30):
 		}
 
 def search_for_files(endpoint,files,threads=5):
-
 	endpoint = helpers.urlify(endpoint)['URL_DIR']
 	search = []
 	found  = []
-
+	
 	for file in files: search.append( endpoint + file )
-
 	values = helpers.run_on_threading(OK,search,threads)
 
 	for value in values:
 		if value['return'] == True:
 			found.append(value['args'][0])
-
 	return found
 
 
@@ -61,14 +54,13 @@ def additional_checks(request):
 
 	if 'http://' in request['url']:
 		flags.append(FLAG_OVER_HTTP)
-
+		
 	if 'Index of' in request['content'] or 'alt="[DIR]"' in request['content']:
 		flags.append(FLAG_DIR_LISTING)
 
 	return flags
 
 def chkurl(url,files_search,timeout):
-
 		request = fetch( helpers.urlify(url)['URL_FILE'] )
 		flags   = additional_checks(request)
 
@@ -79,7 +71,6 @@ def chkurl(url,files_search,timeout):
 		if str(request['code'])[0] == "3":
 			output = Fore.YELLOW + str(request['code'])
 			output += '%s -> %s%s' % (Fore.GREEN,Fore.LIGHTWHITE_EX,request['url'])
-
 
 		if FLAG_OVER_HTTP in flags:
 			output += " %s-[OVER HTTP]%s" % (Fore.RED,Fore.RESET)

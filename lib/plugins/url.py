56ddf43408d681f4a1696079a7abff8ac5d3d62d
BitTheByte <ahmed.ezzat119@outlook.com>
2019-06-09 17:15:51 +0200

    Improved file search

--- lib/plugins/url.py
+++ lib/plugins/url.py
@@ -4,47 +4,57 @@ interesting_files = [f.strip() for f in open("lib/wordlist/interesting_files.txt
 FLAG_OVER_HTTP = 101
 FLAG_DIR_LISTING = 103
 
-def OK(url):
-	if fetch(url)['code'] == 200: return True
-	return False
+
+def is_available(url):
+	response = fetch(url)
+	if response['code'] == 200:
+		return response
+	return 0
 		
-def fetch(url,timeout=30):
+def fetch(url,fast_mode=1,timeout=30):
 	try:
+
+		if fast_mode == 1:
+			request = requests.head(url,timeout=timeout,allow_redirects=True,verify=False)
+			if 'content-length' in request.headers:
+				return {'url': request.url,'code': request.status_code,'length': request.headers['content-length']}
+			else:
+				return fetch(url,fast_mode=2)
+
+		if fast_mode == 2:
+			request = requests.get(url,timeout=timeout,allow_redirects=True,verify=False)
+			return {'url': request.url,'code': request.status_code,'length': len(request.content)}
+
 		request = requests.get(url,timeout=timeout,verify=False)
 		if request.history: 
-			return {
-				'url':  request.history[-1].headers['location'],
-				'code': request.history[-1].status_code,
-				'content': request.content,
-				'headers': request.history[-1].headers
-			}
+			return {'url':  request.history[-1].headers['location'],'code': request.history[-1].status_code,'content': request.content,'headers': request.history[-1].headers}
 
-		return {
-			'url': request.url,
-			'code': request.status_code,
-			'content': request.content,
-			'headers': request.headers
-		}
+		return {'url': request.url,'code': request.status_code,'content': request.content,'headers': request.headers}
 
-	except:
-		return {
-			'url': "",
-			'code': "down",
-			'content': "",
-			'headers': []
-		}
+	except Exception as e:
+		return {'url': "",'code': "down",'content': "",'headers': []}
+
 
 def search_for_files(endpoint,files,threads=5):
 	endpoint = helpers.urlify(endpoint)['URL_DIR']
 	search = []
 	found  = []
+
+	base_response     = fetch(endpoint)['length']
+	base_404_response = fetch(endpoint + "dummy_not_found")['length']
 	
 	for file in files: search.append( endpoint + file )
-	values = helpers.run_on_threading(OK,search,threads)
+	threads_result = helpers.run_on_threading(is_available,search,threads)
 
-	for value in values:
-		if value['return'] == True:
-			found.append(value['args'][0])
+	for result in threads_result:
+		if result['return'] and not result['return']['length'] in (base_response,base_404_response):
+			"""
+			TODO: FIX THIS
+			This can be tricked by 30x redirects
+			host.com/.git -> 302 -> another.com/home
+			return value: another.com/home
+			"""
+			found.append(result['return']['url'])
 	return found
 
 
@@ -60,7 +70,7 @@ def additional_checks(request):
 	return flags
 
 def chkurl(url,files_search,timeout):
-		request = fetch( helpers.urlify(url)['URL_FILE'] )
+		request = fetch( helpers.urlify(url)['URL_FILE'] , fast_mode=0)
 		flags   = additional_checks(request)
 
 		if request['code'] == "down": output = Fore.RED    + "DOWN"
@@ -81,5 +91,4 @@ def chkurl(url,files_search,timeout):
 			files   = search_for_files(url, interesting_files )
 			for file in files:
 				output += "%s\n        |> [INTERESTING FILE]> %s%s" % (Fore.GREEN,Fore.WHITE,file)
-
 		return output
